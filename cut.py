0f0a3d787bb84530a40ba3a6211fe574eac73185
WEINAN CHEN <wzc124@submit-006.acib.production.int.aci.ics.psu.edu>
2022-05-24 16:20:39 -0400

    wrapper function for stride program

--- cut.py
+++ cut.py
@@ -1,11 +1,80 @@
+from ast import parse
+import MDAnalysis
 import numpy as np
-import matplotlib.pyplot as plt
-import gaussian_entanglements as ge
-import clustering_updated as cluster
+#import gaussian_entanglements as ge
+#import clustering_updated as cluster
+import subprocess
+import sys
+import re
+
+
+def parse_stride_output(stride_output):
+    """parse the stride output to find the secondary structure indices"""
+    # use regex expression to find expression of "LOC NAME resname resid - resname resid -"
+    exp = re.findall("LOC \D+ \d{1,5} \- \D+ \d{1,5} \-", stride_output)
+    indices = np.zeros((len(exp), 2))
+    #indices = []
+    for idx, each_line in enumerate(exp):
+        tmp = each_line.split()
+        indices[idx, 0] = int(tmp[3])
+        indices[idx, 1] = int(tmp[6])
+   
+    return indices
+ 
+def stride_wrapper(pdb_file_name: str, pdb_length: int):
+    """A python wrapper that calls the stride program 
+        and obtain the secondary structure indices"""
+
+    proc = subprocess.Popen("stride -o %s"%(pdb_file_name), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc.wait()
+    (stdout, stderr) = proc.communicate()
+
+    # if not normal return
+    if proc.returncode != 0:
+        # if error, print error message and quit
+        sys.exit(stderr)
+    else:
+        #print("success")
+        stride_output = stdout.decode('ascii')
+        parsed_indices = parse_stride_output(stride_output)
+
+        # hard code the size for now, TBD
+        masked_array = np.ones(303)
+        for each_pair in parsed_indices:
+            ss_start_index = int(each_pair[0] - 1)
+            ss_end_index = int(each_pair[1] - 1)
+            masked_array[ss_start_index:ss_end_index] = 0
+        return masked_array
+
+def load_pdb(pdb_file_name: str):
+    """
+    load the pdb struct, input: file_name
+    outputs: atoms, list of residue id, list of residue name
+    """
+    struct = MDAnalysis.Universe(pdb_file_name, format="PDB")
+    protein = struct.select_atoms("all")
+    residue_list = list(protein.residues.resids)
+    resname_list = list(protein.residues.resnames)
+
+    return protein, residue_list, resname_list
 
 def main():
 
-    print("Hello this is new")
+    pdb_file_name = "native.pdb"
+    # the starting cut 
+    n_cut = 1
+    # load the structure into our system
+    protein, resid_list, resname_list = load_pdb(pdb_file_name)
+    n_amino_acid = len(resid_list)
+    n_cut_sites = n_amino_acid - 1
+
+    # a generic wrapper for stride program, TBD
+    protein_mask = stride_wrapper(pdb_file_name, n_amino_acid)
+    #print(protein_mask)
+
+
+
+    
     return
 
 if __name__ == '__main__':
