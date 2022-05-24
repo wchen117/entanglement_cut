71c6a34e77eb5be6f14cd7098b1a97250b45fd8d
WEINAN CHEN <wzc124@submit-006.acib.production.int.aci.ics.psu.edu>
2022-05-24 10:56:45 -0400

    setup vs code

--- cut.py
+++ cut.py
@@ -1,8 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import gaussian_entanglements as ge
+import clustering_updated as cluster
 
 def main():
 
+    print("Hello this is new")
     return
 
 if __name__ == '__main__':
