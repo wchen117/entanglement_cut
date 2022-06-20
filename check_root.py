edcb9137311b76fabba6ce7e6dc706fa82e7b74d
WEINAN CHEN <wzc124@submit-007.acib.production.int.aci.ics.psu.edu>
2022-06-20 14:17:29 -0400

    some exmaple usages of anytree module

--- check_root.py
+++ check_root.py
@@ -1,47 +1,50 @@
-from anytree import Node, RenderTree, LevelOrderGroupIter
+from IndexNode import IndexNode
+from anytree import RenderTree
 
-def get_all_keys(d):
-    for key, value in d.items():
-        yield key
-        if isinstance(value, dict):
-            yield from get_all_keys(value)
 
-# return a list of paths 
-def tranverse_tree(tree):
+def transvers_path(tree):
 
-    print(RenderTree(tree))
-    return
+    """assume the give tree is a root note"""
+
+    big_list = []
+    if tree.is_root:
+        for leaf in tree.leaves:
+            big_list.append(leaf.getIndexPath())
+
+
+    return big_list
+
 
 def build_psuedo_tree():
-    tree = Node("root")
-    a = Node("a", parent = tree)
-    b = Node("b", parent = tree)
-    c = Node("c", parent = tree)
+    tree = IndexNode("root")
+    a = IndexNode("a", parent = tree)
+    b = IndexNode("b", parent = tree)
+    c = IndexNode("c", parent = tree)
 
-    a1 = Node("a1", parent = a)
-    a2 = Node("a2", parent = a)
-    a3 = Node("a3", parent = a)
+    a1 = IndexNode("a1", parent = a)
+    a2 = IndexNode("a2", parent = a)
+    a3 = IndexNode("a3", parent = a)
 
-    b1 = Node("b1", parent = b)
-    b2 = Node("b2", parent = b)
-    b3 = Node("b3", parent = b)
+    b1 = IndexNode("b1", parent = b)
+    b2 = IndexNode("b2", parent = b)
+    b3 = IndexNode("b3", parent = b)
 
-    c1 = Node("b1", parent = c)
-    c2 = Node("b2", parent = c)
-    c3 = Node("b3", parent = c)
+    c1 = IndexNode("c1", parent = c)
+    c2 = IndexNode("c2", parent = c)
+    c3 = IndexNode("c3", parent = c)
 
     return tree
 
 def auto_leaf(node, leaf_num = 3):
 
     for idx in range(leaf_num):
-        new_node = Node("{idx}".format(idx=idx), parent = node)
+        new_node = IndexNode("{idx}".format(idx=idx), parent = node)
 
     return
 
-def auto_build_tree(leaf_num =3, max_level = 2):
+def auto_build_tree(leaf_num =3, max_level = 3):
 
-    tree = Node("root")
+    tree = IndexNode("root")
     
     level = 0;
    
@@ -55,23 +58,14 @@ def auto_build_tree(leaf_num =3, max_level = 2):
             level = level + 1
 
     return tree
-
-def buildTree(treeNode):
-
-    # build a tree with three nodes per level
-    # and max 5 layers
-
-    
-    return treeNode
-
-
  
 def main():
     #tree = build_psuedo_tree();
-    tree = auto_build_tree();
-    #RenderTree(tree)
+    tree = auto_build_tree()
+    print(RenderTree(tree))
     #print(tree)
-    tranverse_tree(tree)
+    all_paths = transvers_path(tree)
+    print(all_paths)
     
 
     return
